857c0a76815ae28e650e76f25b404eb8d3d9c810
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-10-16 16:06:08 +1100

    save code for cli execution, will prob revert to plain hcl file

--- cookbooks/vault/attributes/default.rb
+++ cookbooks/vault/attributes/default.rb
@@ -16,3 +16,14 @@ default[:vault][:hashi_repo]    = {
   enabled:      true,
   pkg_name:     'vault',
 }
+
+default[:vault][:agent_config][:flags][:config]             = '/etc/vault.d/vault.hcl'
+default[:vault][:agent_config][:flags][:exit_after_auth]    = 'false' # default
+default[:vault][:agent_config][:flags][:log_level]          = 'info'  # default
+
+default[:vault][:agent_config][:approle][:role_id_file_path]                   = '/tmp/role_id_file_path'
+default[:vault][:agent_config][:approle][:secret_id_file_path]                 = '/tmp/secret_id_file_path'
+default[:vault][:agent_config][:approle][:remove_secret_id_file_after_reading] = true
+
+default[:vault][:agent_config][:args][:address]   = 'https://vault-public-vault-8860d7cf.f6ac8407.z1.hashicorp.cloud:8200'      # server address - overrides value in config file
+default[:vault][:agent_config][:args][:namespace] = 'admin' # namespace for commands
