e8a81edce07c2c27f38f259739c9457ef9e7f0ea
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-11-02 20:47:39 +1100

    turn off support for agent listener

--- cookbooks/vault/metadata.rb
+++ cookbooks/vault/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'leif.eriksen.au+chef-vault-github@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Vault client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.48'
+version '0.4.49'
 chef_version '>= 15.0'
 source_url 'https://github.com/leriksen/chef-vault-client'
 issues_url 'https://github.com/leriksen/chef-vault-client/issues'
