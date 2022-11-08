2b5f1e102eec4b9db4cd7b12e18f6f6234961604
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-11-08 12:22:05 +1100

    use the data in the wrapped cert to create the auth entries

--- cookbooks/vault/spec/vault-agent-configure_spec.rb
+++ cookbooks/vault/spec/vault-agent-configure_spec.rb
@@ -1,15 +1,5 @@
 describe "vault::vault-agent-configure" do
   platform 'redhat', '8'
 
-  before(:each) do
-    stub_data_bag_item("vault", "approle").and_return(
-      {
-        id: 'approle',
-        role_id: 'role_id',
-        secret_id: 'secret_id',
-      }
-    )
-  end
-
   it { is_expected.to create_template('/etc/vault.d/vault-agent.hcl') }
 end
