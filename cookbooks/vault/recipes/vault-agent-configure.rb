e87f51a7bdf020222dcd70084318cc27b8c53ef0
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-11-08 12:20:28 +1100

    use the data in the wrapped cert to create the auth entries

--- cookbooks/vault/recipes/vault-agent-configure.rb
+++ cookbooks/vault/recipes/vault-agent-configure.rb
@@ -1,13 +1,3 @@
-secrets = data_bag_item('vault', 'approle')
-
-file node[:vault][:agent_config][:role_id] do
-  content secrets['role_id']
-end
-
-file node[:vault][:agent_config][:secret_id] do
-  content secrets['secret_id']
-end
-
 template node[:vault][:agent_config][:config] do
   source 'vault-agent.hcl.erb'
   owner node[:accounts][:vault][:unix_user]
